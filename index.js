fe6f114860708e69e6d9dc81f45ded49e8734bac
Tanmay <58038326+tanmaymishra91@users.noreply.github.com>
2024-09-28 11:03:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@
 /_______  /\___|_  /|___|  \___/\____|__  / 
         \/       \/                     \/  
                     
-DISCORD :  https://discord.com/invite/xQF9f9yUEM                   
-YouTube : https://www.youtube.com/@GlaceYT                         
+DISCORD :  https://discord.com/invite/bless-bot                   
+YouTube : https://www.youtube.com/-                        
                                                                        
 ☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
 
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing bless-bot", "💖 I love bless-bot", "🫶 Join discord.gg/bless-bot"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
